752dc30a7921ce8d3f161d9d848b19ceb8bb930b
Michal Fapso <michal.fapso@gmail.com>
2025-05-06 00:46:16 +0200

    website/astro.config.mjs: base url set for github pages

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -2,4 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Set the site property to your full GitHub Pages URL
+  site: 'https://michalfapso.github.io', // Replace with your actual username if different
+  // Set the base path to your repository name
+  base: '/vlk_uradne_tabule/', // Make sure this matches your repository name!
+});
