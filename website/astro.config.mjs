db9f945667efca357e8cb5c5ee425a59e433abde
Michal Fapso <michal.fapso@gmail.com>
2025-06-22 00:21:48 +0200

    website: added tailwind css

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   // Set the site property to your full GitHub Pages URL
-  site: 'https://michalfapso.github.io', // Replace with your actual username if different
+  // Replace with your actual username if different
+  site: 'https://michalfapso.github.io',
   // Set the base path to your repository name
-  base: '/vlk_uradne_tabule/', // Make sure this matches your repository name!
+  // Make sure this matches your repository name!
+  base: '/vlk_uradne_tabule/',
+  integrations: [tailwind()]
 });
---
+++ website/tailwind.config.mjs
@@ -0,0 +1,10 @@
+// tailwind.config.mjs
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+};
